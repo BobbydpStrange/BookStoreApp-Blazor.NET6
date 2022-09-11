6805b4f589f855b7c094fe6fa5230b99d2efaa0b
BobbydpStrange <51657838+BobbydpStrange@users.noreply.github.com>
2022-09-11 17:26:02 -0600

    Completed Book API Setup

--- BookStoreApp.API/Data/Book.cs
+++ BookStoreApp.API/Data/Book.cs
@@ -6,12 +6,19 @@ namespace BookStoreApp.API.Data
     public partial class Book
     {
         public int Id { get; set; }
+
         public string? Title { get; set; }
+
         public int? Year { get; set; }
+
         public string Isbn { get; set; } = null!;
+
         public string? Summary { get; set; }
+
         public string? Image { get; set; }
+
         public decimal? Price { get; set; }
+
         public int? Authorid { get; set; }
 
         public virtual Author? Author { get; set; }
